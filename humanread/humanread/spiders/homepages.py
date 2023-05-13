65e40d6f8e85645e70571e7b77699b8a37bf405f
kalleolumets <olumetskalle@gmail.com>
2023-05-13 08:46:49 +0300

    save to file

--- humanread/humanread/spiders/homepages.py
+++ humanread/humanread/spiders/homepages.py
@@ -6,5 +6,17 @@ class HomepagesSpider(scrapy.Spider):
     allowed_domains = ["python.org"]
     start_urls = ["http://python.org/"]
 
+    def start_requests(self):
+        urls = self.start_urls # Replace with your desired website
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
     def parse(self, response):
-        pass
+        page = response.url.split("/")[-2]
+        filename = f"{page}.txt"
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
+
+        for link in response.css('a::attr(href)').getall():
+            yield response.follow(link, self.parse)
