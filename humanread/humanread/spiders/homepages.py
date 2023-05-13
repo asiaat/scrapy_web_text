347ad33f401c963f9800f7ecfcc1c04b026e7e38
kalleolumets <olumetskalle@gmail.com>
2023-05-13 16:53:06 +0300

    save scraping results as one file

--- humanread/humanread/spiders/homepages.py
+++ humanread/humanread/spiders/homepages.py
@@ -1,10 +1,11 @@
 import scrapy
-
+from bs4 import BeautifulSoup
 
 class HomepagesSpider(scrapy.Spider):
     name = "homepages"
     allowed_domains = ["python.org"]
-    start_urls = ["http://python.org/"]
+    start_urls = ["https://python.org/"]
+    filename = f"output/crawl1.txt"
 
     def start_requests(self):
         urls = self.start_urls # Replace with your desired website
@@ -12,11 +13,15 @@ class HomepagesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"{page}.txt"
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+        #page = response.url.split("/")[-2]
+        #filename = f"output/{page}.txt"
+        
+        soup = BeautifulSoup(response.text, 'html.parser')
+        text = soup.get_text()
+        
+        with open(self.filename, 'a') as f:
+            f.write(text)
+        self.log(f'Saved file {self.filename}')
 
         for link in response.css('a::attr(href)').getall():
             yield response.follow(link, self.parse)
