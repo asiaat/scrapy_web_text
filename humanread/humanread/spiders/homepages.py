9e66b25ad056368a8a9aa6e394ad08cfd6da4089
kalleolumets <olumetskalle@gmail.com>
2023-05-14 11:06:00 +0300

    new way to make filenames

--- humanread/humanread/spiders/homepages.py
+++ humanread/humanread/spiders/homepages.py
@@ -4,17 +4,21 @@ from bs4 import BeautifulSoup
 class HomepagesSpider(scrapy.Spider):
     name = "homepages"
     allowed_domains = ["python.org"]
-    start_urls = ["https://python.org/"]
+    start_urls = ["https://pypi.org/project/pandas/", "https://pypi.org/project/unstructured/"]
     filename = f"output/crawl2.txt"
+    outputdir = "output"
+    homepage=""
 
     def start_requests(self):
         urls = self.start_urls # Replace with your desired website
+        
         for url in urls:
+            self.homepage=url.split("//")[1].replace("/","_")
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        #page = response.url.split("/")[-2]
-        #filename = f"output/{page}.txt"
+        page = self.homepage
+        self.filename = f"output/{page}.txt"
         
         soup = BeautifulSoup(response.text, 'html.parser')
         text = soup.get_text()
